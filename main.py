6dac93ebd5918595a8d03f231be3a17a30c41ed6
B-Ricey763 <39998963+B-Ricey763@users.noreply.github.com>
2021-03-02 16:45:33 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,58 +2,52 @@ import sys
 import pymunk
 import pygame
 from pygame import *
-from pymunk import body
-from pymunk.shapes import Shape
 
-def create_circle(space):
+def create_circ(space, pos, mass):
     body = pymunk.Body(0, 0, body_type = pymunk.Body.DYNAMIC)
-    body.position = (400, 0)
-
+    body.position = pos
     circ = pymunk.Circle(body, RADIUS)
-    circ.mass = 10
-
+    circ.mass = mass
     space.add(body, circ)
-
     return circ
 
-def create_floor(space):
+def create_static_seg(space, ptA, ptB, width):
     body = pymunk.Body(0, 0, body_type = pymunk.Body.STATIC)
+    seg = pymunk.Segment(body, ptA, ptB, width)
+    space.add(body, seg)
+    return seg
 
-    box = pymunk.Segment(body, (0, 700), (800, 700), 2)
-    
-    space.add(body, box)
-
-    return box
-
-def draw_circles(circs):
+def draw_circs(circs):
     for circ in circs:
         pos_x = int(circ.body.position.x)
         pox_y = int(circ.body.position.y)
 
         pygame.draw.circle(screen, BODY_COLOR, (pos_x, pox_y), RADIUS)
 
-def draw_floor(box):
-    pos1 = (int(box.a[0]), int(box.a[1]))
-    pos2 = (int(box.b[0]), int(box.b[1]))
-
+def draw_floor(seg):
+    pos1 = (int(seg.a[0]), int(seg.a[1]))
+    pos2 = (int(seg.b[0]), int(seg.b[1]))
     pygame.draw.line(screen, BODY_COLOR, pos1, pos2, 2)
 
-pygame.init()
-
-SIZE = 800, 800
+SIZE = 900, 900
 FILL_COLOR = 200, 200, 200
 BODY_COLOR = 0, 0, 0
 RADIUS = 80
+GRAVITY = 100
+
+pygame.init()
 
 screen = pygame.display.set_mode(SIZE)
 clock = pygame.time.Clock()
 space = pymunk.Space()
-space.gravity = 0, 500
+space.gravity = 0, GRAVITY
 
 circs = []
-circs.append(create_circle(space))
+circs.append(create_circ(space, (400, 0), 10))
 
-floor = create_floor(space)
+floor = create_static_seg(space, 
+    (0, SIZE[1] - 10), (SIZE[0], SIZE[1] - 10), 
+    2)
 
 # Game loop
 while True:
@@ -63,7 +57,7 @@ while True:
             sys.exit()
 
     screen.fill(FILL_COLOR)
-    draw_circles(circs)
+    draw_circs(circs)
     draw_floor(floor)
     space.step(1/50)
     pygame.display.update()
